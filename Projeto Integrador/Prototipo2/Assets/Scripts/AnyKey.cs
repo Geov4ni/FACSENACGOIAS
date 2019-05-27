dee2f5ba2e6b89bd44d3af66f9ad468de625daaa
Geov4ni <Geov4ni@users.noreply.github.com>
2019-05-27 20:33:09 -0300

    Atualização Protótipo do PI

--- Projeto Integrador/Prototipo2/Assets/Scripts/AnyKey.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class AnyKey : MonoBehaviour {
-    void Update() {
-        if (Input.anyKey) { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); }
-    }
-}
--- Projeto Integrador/Prototipo2/Assets/Scripts/Managers/AudioManager.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class AudioManager : MonoBehaviour {
-    private void Awake() => GetComponent<Maestria>().NewSoundtrack("Music/Rei do Gado");
-
-}
